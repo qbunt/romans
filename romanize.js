470994cb50a9715bbd6699b34929e0c8dbcfa2fd
Jeremy Bunting <buntingj@gmail.com>
2016-05-15 23:45:32 -0700

    clarity edits

--- romanize.js
+++ romanize.js
@@ -1,47 +1,57 @@
 "use strict";
+var roman = (function(){
+    var roman_map = {
+        'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1};
 
-var roman_map = {
-    'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1};
+    var allChars = [];
+    var allNumerals = [];
 
-var allChars = [];
-var allNumerals = [];
-function deriveCollections() {
-    for(var character in roman_map){
-        allChars.push(character);
-    }
+    /**
+     * splits the object hash into several useful parts
+     */
+    function deriveCollections() {
+        for(var character in roman_map){
+            allChars.push(character);
+        }
 
-    for(var numeral in roman_map){
-        allNumerals.push(roman_map[numeral]);
-    }
-}
-
-function romanize(decimal) {
-    console.time('roman:');
-    var roman = '';
-    for (var i = 0; i < allChars.length; i++) {
-        while(decimal >= allNumerals[i]){
-            decimal -= allNumerals[i];
-            roman += allChars[i];
+        for(var numeral in roman_map){
+            allNumerals.push(roman_map[numeral]);
         }
     }
-    console.timeEnd('roman:');
-    return roman;
-}
 
-function deromanize(romanStr) {
-    console.time('arabicize:');
-    var romanString = romanStr.toUpperCase();
-    var arabic = 0;
-    var iteration = romanString.length;
-    while(iteration--){
-        var cumulative = roman_map[romanString[iteration]];
-        if ( cumulative < roman_map[romanString[iteration+1]] ){
-            arabic -= cumulative;
-        } else {
-            arabic += cumulative;
+    function romanize(decimal) {
+        console.time('roman:');
+        var roman = '';
+        for (var i = 0; i < allChars.length; i++) {
+            while(decimal >= allNumerals[i]){
+                decimal -= allNumerals[i];
+                roman += allChars[i];
+            }
         }
+        console.timeEnd('roman:');
+        return roman;
     }
-    console.timeEnd('arabicize:');
-    return arabic;
-}
-deriveCollections();
+
+    function deromanize(romanStr) {
+        console.time('arabicize:');
+        var romanString = romanStr.toUpperCase();
+        var arabic = 0;
+        var iteration = romanString.length;
+        while(iteration--){
+            var cumulative = roman_map[romanString[iteration]];
+            if ( cumulative < roman_map[romanString[iteration+1]] ){
+                arabic -= cumulative;
+            } else {
+                arabic += cumulative;
+            }
+        }
+        console.timeEnd('arabicize:');
+        return arabic;
+    }
+    deriveCollections();
+
+    return {
+        deromanize: deromanize,
+        romanize: romanize
+    }
+})();
--- test/main.js
+++ test/main.js
@@ -1,14 +1,14 @@
 describe('check for parity on input & output', function () {
     it("should return the same value on conversion", function () {
         var myRoman = "CCLIV";
-        var myArabic = deromanize(myRoman);
-        expect(myArabic).toEqual(deromanize(myRoman))
+        var myArabic = roman.deromanize(myRoman);
+        expect(myArabic).toEqual(roman.deromanize(myRoman))
     });
     
     it("should gracefully handle mixed cases", function () {
         var myStrings = ["mXvIi", "dcvii", "mmILV"];
         var myConversions = myStrings.map(function (item) {
-            return deromanize(item);
+            return roman.deromanize(item);
         });
         for (var i = 0; i < myConversions.length; i++) {
             expect(typeof myConversions[i]).toBe("number")
